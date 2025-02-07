37f4b3258a7bdc181b405d63b9e9c60d026bae71
win <mr.vjcspy@gmail.com>
2025-02-08 05:31:31 +0700

    feat: init

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace NDBotUI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NDBotUI.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
