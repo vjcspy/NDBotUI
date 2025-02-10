6d6e0d334e151b2e1e77f61312ca050758d340d4
win <mr.vjcspy@gmail.com>
2025-02-10 22:14:10 +0700

    chore

---
+++ UI/Game/MementoMori/Views/MoriTwoColumnSpliter.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NDBotUI.UI.Game.MementoMori.Views;
+
+public partial class MoriTwoColumnSpliter : UserControl
+{
+    public MoriTwoColumnSpliter()
+    {
+        InitializeComponent();
+    }
+}
