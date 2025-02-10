833ba5d28c8e2a8a74feb3084e2bebe0f03838aa
win <mr.vjcspy@gmail.com>
2025-02-10 22:01:37 +0700

    chore: StateToColorConverter

---
+++ UI/Emulator/Converters/StateToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using AdvancedSharpAdbClient.Models;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace NDBotUI.UI.Emulator.Converters;
+
+public class StateToColorConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            DeviceState.Offline => Brushes.Red,
+            DeviceState.Online => Brushes.Green,
+            _ => Brushes.Gray
+        };
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
