bd15fff76bc3a39df12f21e6033dc02a25f8db91
win <mr.vjcspy@gmail.com>
2025-02-11 12:16:20 +0700

    chore

---
+++ Modules/Game/MementoMori/Store/MoriAction.cs
@@ -0,0 +1,13 @@
+﻿using NDBotUI.Modules.Shared.EventManager;
+
+namespace NDBotUI.Modules.Game.MementoMori.Store;
+
+public class MoriAction
+{
+    public enum Type
+    {
+        TriggerScanCurrentScreen,
+    }
+    
+    public static EventActionFactory<object?> TriggerScanCurrentScreen = new(Type.TriggerScanCurrentScreen);
+}
