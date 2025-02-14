39da79b9d4eb5a48a019f6978daa870e50b6bb83
win <mr.vjcspy@gmail.com>
2025-02-14 14:24:13 +0700

    chore

---
+++ Modules/Core/Helper/ScreenCapture.cs
@@ -0,0 +1,53 @@
+﻿// using System;
+// using System.Drawing;
+// using System.Drawing.Imaging;
+// using System.Runtime.InteropServices;
+//
+// namespace NDBotUI.Modules.Core.Helper;
+//
+// public class ScreenCapture
+// {
+//     [DllImport("user32.dll")]
+//     private static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, uint nFlags);
+//
+//     [DllImport("user32.dll")]
+//     private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+//
+//     /// <summary>
+//     ///     Chụp ảnh màn hình cửa sổ ứng dụng theo tên và lưu vào file.
+//     /// </summary>
+//     /// <param name="windowTitle">Tên cửa sổ ứng dụng cần chụp.</param>
+//     /// <param name="outputPath">Đường dẫn lưu ảnh chụp màn hình.</param>
+//     public static void TakeScreenshot(string windowTitle, string outputPath)
+//     {
+//         var hWnd = FindWindow(null, windowTitle);
+//         if (hWnd == IntPtr.Zero)
+//         {
+//             Console.WriteLine("Không tìm thấy cửa sổ: " + windowTitle);
+//             return;
+//         }
+//
+//         // Giả định kích thước cửa sổ, cần chỉnh sửa nếu cửa sổ lớn hơn
+//         var width = 1920;
+//         var height = 1080;
+//
+//         using (var bmp = new Bitmap(width, height))
+//         {
+//             using (var g = Graphics.FromImage(bmp))
+//             {
+//                 var hdc = g.GetHdc();
+//                 var success = PrintWindow(hWnd, hdc, 0);
+//                 g.ReleaseHdc(hdc);
+//
+//                 if (!success)
+//                 {
+//                     Console.WriteLine("Không thể chụp ảnh cửa sổ.");
+//                     return;
+//                 }
+//             }
+//
+//             bmp.Save(outputPath, ImageFormat.Png);
+//             Console.WriteLine($"Ảnh chụp màn hình đã lưu tại: {outputPath}");
+//         }
+//     }
+// }
--- Modules/Game/MementoMori/Store/Effects/EffectTemplate.cs
+++ Modules/Game/MementoMori/Store/Effects/EffectTemplate.cs
@@ -1,9 +1,6 @@
-﻿using System.Diagnostics;
-using System.Reactive.Linq;
+﻿using System.Reactive.Linq;
 using System.Threading.Tasks;
-using NDBotUI.Modules.Core.Extensions;
 using NDBotUI.Modules.Core.Helper;
-using NDBotUI.Modules.Game.MementoMori.Helper;
 using NDBotUI.Modules.Shared.Emulator.Services;
 using NDBotUI.Modules.Shared.EventManager;
 using NLog;
@@ -27,30 +24,33 @@ public class EffectTemplate
         // Logger.Debug($"Screen resolution: {resolution}");
 
         /* Test find template */
-        if (TemplateImageDataHelper.IsLoaded &&
-            TemplateImageDataHelper.TemplateImageData[MoriTemplateKey.StartStartButton].EmuCVMat is
-                { } templateMat)
-        {
-            var screenshot = await emulator.TakeScreenshotAsync();
-            if (screenshot is null) return CoreAction.Empty;
-
-            var stopwatch = Stopwatch.StartNew();
-            var screenshotMat = screenshot.ToEmguMat();
-            stopwatch.Stop();
-            Logger.Info($"Screenshot to Emgu Mat took {stopwatch.ElapsedMilliseconds} ms");
-            // ImageFinderEmguCV.SaveMatToFile(screenshotMat, "screenshot1.png");
-            // ImageFinderEmguCV.SaveMatToFile(templateMat, "template1.png");
-            // var point = ImageFinderEmguCV.FindTemplateORB(screenshotMat, templateMat);
-            var point = ImageFinderEmguCV.FindTemplateMatPoint(screenshotMat, templateMat, "screenshot_marked.png");
-            if (point != null)
-                Logger.Info($"Point is {point.Value.X}, {point.Value.Y}");
-            else
-                Logger.Info("Point is null");
-        }
-        else
-        {
-            Logger.Info("TemplateImageDataHelper not loaded");
-        }
+        // if (TemplateImageDataHelper.IsLoaded &&
+        //     TemplateImageDataHelper.TemplateImageData[MoriTemplateKey.StartStartButton].EmuCVMat is
+        //         { } templateMat)
+        // {
+        //     var screenshot = await emulator.TakeScreenshotAsync();
+        //     if (screenshot is null) return CoreAction.Empty;
+        //
+        //     var stopwatch = Stopwatch.StartNew();
+        //     var screenshotMat = screenshot.ToEmguMat();
+        //     stopwatch.Stop();
+        //     Logger.Info($"Screenshot to Emgu Mat took {stopwatch.ElapsedMilliseconds} ms");
+        //     // ImageFinderEmguCV.SaveMatToFile(screenshotMat, "screenshot1.png");
+        //     // ImageFinderEmguCV.SaveMatToFile(templateMat, "template1.png");
+        //     // var point = ImageFinderEmguCV.FindTemplateORB(screenshotMat, templateMat);
+        //     var point = ImageFinderEmguCV.FindTemplateMatPoint(screenshotMat, templateMat, "screenshot_marked.png");
+        //     if (point != null)
+        //         Logger.Info($"Point is {point.Value.X}, {point.Value.Y}");
+        //     else
+        //         Logger.Info("Point is null");
+        // }
+        // else
+        // {
+        //     Logger.Info("TemplateImageDataHelper not loaded");
+        // }
+        
+        /* Dùng API Windows để take screenshot nhưng bị lệch và không hoạt động khi minimized */
+        // ScreenCapture.TakeScreenshot("BlueStacks App Player", ImageHelper.GetImagePath("BlueStacks-Screenshot.png"));
 
 
         return CoreAction.Empty;
